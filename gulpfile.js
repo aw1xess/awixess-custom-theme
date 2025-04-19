5e468115b7fa43283d7c3942d5bf3cf2b7e8b958
aw1xess <yariksvirshchuk@gmail.com>
2025-04-19 11:22:20 +0300

    Initialize gulp with scss

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+const paths = {
+  scss: {
+    src: 'assets/scss/**.*',
+    dest: 'assets/',
+  },
+};
+
+function compileSCSS() {
+  return gulp.src(paths.scss.src).pipe(sass().on('error', sass.logError)).pipe(gulp.dest(paths.scss.dest));
+}
+
+function watchFiles() {
+  gulp.watch(paths.scss.src, compileSCSS);
+}
+
+exports.default = gulp.series(compileSCSS, watchFiles);
